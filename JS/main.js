72ce6d33db572a30f09d44cd0da50c0d4940ba2b
nukaterrapin7 <pythonism007@gmail.com>
2022-09-19 15:49:51 -0500

    Adds HTML, CSS, AND JS to start

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+/*---Constants---*/
+const WIN
+
+/*---Variables---*/
+let board, winner
+
+/*---Cached Elements---*/
+const startBtnEl = document.querySelector('sButton');
+const resetBtnEl = document.querySelector('rButton');
+const boardEl = document.getElementById('board');
+
+/*---Event Listeners---*/
+startBtnEl.addEventListener('click', handleStartClick);
+resetBtnEl.addEventListener('click', handleResetClick);
+boardEl.addEventListener('click', handleBoardClick);
+
+/*---Functions---*/
+function init() {
+    turn = 1
+    board = [null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null]
+    winner = null
+    render()
+};
+
+function handleStartClick() {
+    init()
+}
