934be69d1ab92714044c229d9558922fc62e7021
nukaterrapin7 <pythonism007@gmail.com>
2022-09-20 19:43:26 -0500

    Adds function and fixes card sizes

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,106 @@
-/*---Constants---*/
-const WIN
-const cardArr = [
-    {
-        name: 'Uranus',
-        img: card.uranus
-    }
-]
+const board = document.querySelector('.board');
+const tileArr = [
+    {name: 'Alien', img: 'images/Alien.png'},
+    {name: 'Alien', img: 'images/Alien.png'},
+    {name: 'Astronaut', img: 'images/Astronaut.png'},
+    {name: 'Astronaut', img: 'images/Astronaut.png'},
+    {name: 'Big Dipper', img: 'images/Big Dipper.png'},
+    {name: 'Big Dipper', img: 'images/Big Dipper.png'},
+    {name: 'Black Hole', img: 'images/Black Hole.png'},
+    {name: 'Black Hole', img: 'images/Black Hole.png'},
+    {name: 'Comet', img: 'images/Comet.png'},
+    {name: 'Comet', img: 'images/Comet.png' },
+    {name: 'Earth', img: 'images/Earth.png'},
+    {name: 'Earth', img: 'images/Earth.png'},
+    {name: 'Galaxy', img: 'images/Galaxy.png' },
+    {name: 'Galaxy', img: 'images/Galaxy.png' },
+    {name: 'ISS', img: 'images/ISS.png'},
+    {name: 'ISS', img: 'images/ISS.png'},
+    {name: 'Jupiter', img: 'images/Jupiter.png'},
+    {name: 'Jupiter', img: 'images/Jupiter.png'},
+    {name: 'Mars', img: 'images/Mars.png'},
+    {name: 'Mars', img: 'images/Mars.png'},
+    {name: 'Mercury', img: 'images/Mercury.png'},
+    {name: 'Mercury', img: 'images/Mercury.png'},
+    {name: 'Moon', img: 'images/Moon.png'},
+    {name: 'Moon', img: 'images/Moon.png'},
+    {name: 'Neptune', img: 'images/Neptune.png'},
+    {name: 'Neptune', img: 'images/Neptune.png'},
+    {name: 'Observatory', img: 'images/Observatory.png'},
+    {name: 'Observatory', img: 'images/Observatory.png'},
+    {name: 'Rocket Ship', img: 'images/Rocket Ship.png'},
+    {name: 'Rocket Ship', img: 'images/Rocket Ship.png'},
+    {name: 'Satellite', img: 'images/Satellite.png'},
+    {name: 'Satellite',  img: 'images/Satellite.png'},
+    {name: 'Saturn', img: 'images/Saturn.png'},
+    {name: 'Saturn', img: 'images/Saturn.png'},
+    {name: 'Solar System', img: 'images/Solar System.png'},
+    {name: 'Solar System', img: 'images/Solar System.png'},
+    {name: 'Space Rover', img: 'images/Space Rover.png'},
+    {name: 'Space Rover', img: 'images/Space Rover.png'},
+    {name: 'Star', img: 'images/Star.png'},
+    {name: 'Star', img: 'images/Star.png'},
+    {name: 'Sun', img: 'images/Sun.png'},
+    {name: 'Sun', img: 'images/Sun.png'},
+    {name: 'Telescope', img: 'images/Telescope.png'},
+    {name: 'Telescope', img: 'images/Telescope.png'},
+    {name: 'UFO', img: 'images/UFO.png'},
+    {name: 'UFO', img: 'images/UFO.png'},
+    {name: 'Uranus', img: 'images/Uranus.png'},
+    {name: 'Uranus', img: 'images/Uranus.png'}
+];
+var tilesPicked = [];
+var tilesPickedId = [];
+var tilesMatched = [];
 
-/*---Variables---*/
-let board, winner
 
 /*---Cached Elements---*/
-let startBtnEl = document.querySelector('');
-let resetBtnEl = document.querySelector('rButton');
-let boardEl = document.getElementById('board');
+let startBtnEl = button.start
+
+// let resetBtnEl = document.querySelector('rButton');
+// let boardEl = document.getElementById('board');
 
 /*---Event Listeners---*/
-startBtnEl.addEventListener('click', handleStartClick);
-resetBtnEl.addEventListener('click', handleResetClick);
-boardEl.addEventListener('click', handleBoardClick);
+startBtnEl.addEventListener('click', createBoard());
+// resetBtnEl.addEventListener('click', handleResetClick);
+// boardEl.addEventListener('click', handleBoardClick);
 
-/*---Functions---*/
-function init() {
-    turn = 1
-    board = [null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null,null, null,null]
-    winner = null
-    render()
+
+
+tileArr.sort(() => 0.5 - Math.random());
+
+function createBoard() {
+    for (let i = 0; i < tileArr.length; i++) {
+        var tile = document.createElement('img')
+        tile.setAttribute('src', 'images/card back.png')
+        tile.setAttribute('data-id', i)
+        tile.addEventListener('click', flip)
+        board.appendChild(tile)
+    }
 };
 
-function handleStartClick() {
-    init()
-}
+function checkMatch() {
+    var tiles = document.querySelectorAll('img')
+    const firstPick = tilesPickedId[0]
+    const secondPick = tilesPickedId[1]
+    if (tilesPicked[0] === tilesPicked[1]) {
+        tilesMatched.push(tilesPicked)
+    } else {
+        tiles[firstPick].setAttribute('src', 'images/card back.png')
+        tiles[secondPick].setAttribute('src', 'images/card back.png')
+    }
+    tilesPicked = []
+    tilesPickedId = []
+};
+
+function flip() {
+    var tileID = this.getAttribute('data-id')
+    tilesPicked.push(tileArr[tileID].name)
+    tilesPickedId.push(tileID)
+    this.setAttribute('src', tileArr[tileID].img)
+    if (tilesPicked.length ===2) {
+        setTimeout(checkMatch, 800)
+    }
+};
+
+// createBoard()
